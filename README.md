078388b8386522b1652270ec0ed08eaeec15a147
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-06-16 14:24:06 -0300

    Data import

---
+++ README.md
@@ -0,0 +1,28 @@
+# Bot de recolección de recursos Grepolis
+
+Bot de recoleccion grepolis, en fase de desarrollo, pero funcional
+
+Es necesario tener el codigo fuente y asociar como extension de desarrollo en el navegador
+
+## Configurar bot
+
+Cambiar los siguientes parametros en 'data.js'
+
+- numeroAldeas: Numero aldeas activas en la isla. (Se toma para todas las ciudades)
+- tiempoRecoleccion: Tiempo de recoleccion en milisegundos. (Se toma para todas las ciudades)
+- opcionRecoleccion: Del 1 - 4 eligue la opcion de recoleccion
+- h : un codigo que expira y no se para que funciona, se consigue mirando los endpoints de grepolis
+- ciudadesConAldeas: Es una lista de las ciudades con aldeas
+    - nombreCiudad: Es opcional, para entendimiento propio
+    - codigoAldeaInicial: codigo de la aldea con codigo mas pequeño (Inspeccionar elementos).
+    - codigoCiudad:  codigo de la ciudad (Inspeccionar elementos).
+
+## Iniciar bot
+
+Oprima el boton de recolectar
+
+## Pausar bot
+
+Recargar la pagina (En desarrollo algo mas friendly)
+
+**Note: Es posible jugar en la pantalla por ser una extension, y combinar con otras extensiones sin problema**
--- contentScript.js
+++ contentScript.js
@@ -1,151 +1,49 @@
-// El numero de la aldea inicial debe ser el menor
-const data = {
-  numeroAldeas: 6,
-  opcionRecoleccion: 1,
-  ciudadesConAldeas: [
-    {
-      nombreCiudad: "Jam3",
-      codigoAldeaInicial: 985,
-      codigoCiudad: 4104,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam5",
-      codigoAldeaInicial: 1171,
-      codigoCiudad: 5574,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam8",
-      codigoAldeaInicial: 1555,
-      codigoCiudad: 4964,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam9",
-      codigoAldeaInicial: 1519,
-      codigoCiudad: 4951,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam11",
-      codigoAldeaInicial: 1315,
-      codigoCiudad: 4313,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam14",
-      codigoAldeaInicial: 1729,
-      codigoCiudad: 5682,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam15",
-      codigoAldeaInicial: 1747,
-      codigoCiudad: 5606,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam16",
-      codigoAldeaInicial: 1009,
-      codigoCiudad: 3326,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam17",
-      codigoAldeaInicial: 1087,
-      codigoCiudad: 4401,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam18",
-      codigoAldeaInicial: 1735,
-      codigoCiudad: 5616,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam19",
-      codigoAldeaInicial: 1801,
-      codigoCiudad: 8418,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam20",
-      codigoAldeaInicial: 1375,
-      codigoCiudad: 5483,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam21",
-      codigoAldeaInicial: 2179,
-      codigoCiudad: 6925,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam22",
-      codigoAldeaInicial: 1735,
-      codigoCiudad: 8427,
-      active: false,
-    },
-    {
-      nombreCiudad: "Jam23",
-      codigoAldeaInicial: 1375,
-      codigoCiudad: 5477,
-      active: false,
-    },
-    {
-      nombreCiudad: "Jam24",
-      codigoAldeaInicial: 1267,
-      codigoCiudad: 6020,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam25",
-      codigoAldeaInicial: 1801,
-      codigoCiudad: 8436,
-      active: false,
-    },
-    {
-      nombreCiudad: "Jam26",
-      codigoAldeaInicial: 2017,
-      codigoCiudad: 8468,
-      active: true,
-    },
-    {
-      nombreCiudad: "Jam27",
-      codigoAldeaInicial: 1513,
-      codigoCiudad: 4841,
-      active: true,
-    },
-  ],
-};
-
-(() => {
+(async () => {
+  
+  let data = await fetch(chrome.runtime.getURL('/data.json'));
+  data = await data.json();
+  
   //Insetar boton en la interfaz
   const injectElement = document.createElement("button");
   injectElement.innerHTML = "Recolectar aldeas";
+  injectElement.id = "botonIniciarRecoleccion";
   injectElement.style.cssText +=
-    "position:absolute;bottom:30px;left:10px;z-index:1000";
+    "position:absolute;bottom:80px;left:10px;z-index:1000";
   document.body.appendChild(injectElement);
 
-  injectElement.addEventListener("click", recolectarRecursosCiudades);
+  injectElement.addEventListener("click", recolectarRecursos);
 
-  function recolectarRecursosCiudades() {
-    const { ciudadesConAldeas } = data;
-    
-    ciudadesConAldeas.forEach(async (ciudad) => {
-      await recolectarRecursosAldeasCiudad(ciudad);
-    });
+  async function recolectarRecursos() {
+    const { ciudadesConAldeas, tiempoRecoleccion } = data;
+    document.getElementById("botonIniciarRecoleccion").innerHTML = "Recolectando aldeas...";
+    // El tiempo que se pierde esperando entre recoleccion para evitar ban
+    let tiempoGastado = ciudadesConAldeas.length * 6 * 60;
+    await recolectarRecursosCiudades();
+    setInterval(recolectarRecursosCiudades, tiempoRecoleccion - tiempoGastado);
   }
 
-  //TODO: recolectar recursos cada 10 min despues de oprimir el boton
+  async function recolectarRecursosCiudades() {
+    const { ciudadesConAldeas } = data;
+
+    for (const ciudad of ciudadesConAldeas) {
+      await recolectarRecursosAldeasCiudad(ciudad);
+    }
+  }
 
   const recolectarRecursosAldeasCiudad = async (ciudad) => {
     const { numeroAldeas } = data;
+
     for (let index = 0; index < numeroAldeas; index++) {
       await recolectarRecursosAldea(ciudad, index);
     }
   };
+
+  //TODO: LLevar a coolfunctions
+  function delaySeconds(seconds) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, seconds * 1000);
+    });
+  }
 
   const recolectarRecursosAldea = async (ciudad, index) => {
     const { codigoAldeaInicial, codigoCiudad, active } = ciudad;
@@ -154,6 +52,9 @@ const data = {
     if (!active) {
       return;
     }
+
+    //Espera entre recoleccion, para evitar ban
+    await delaySeconds(1);
 
     const json = {
       model_url: "FarmTownPlayerRelation/52287",
@@ -171,9 +72,8 @@ const data = {
 
     datos.append("json", JSON.stringify(json));
 
-    //TODO: Si es variable h, de donde se consigue?
     await fetch(
-      `https://es108.grepolis.com/game/frontend_bridge?town_id=${codigoCiudad}&action=execute&h=5b5431dc294833019a2fe8896edd472f07041e2a`,
+      `https://es108.grepolis.com/game/frontend_bridge?town_id=${codigoCiudad}&action=execute&h=${data.h}`,
       {
         method: "POST",
         headers: {
@@ -185,4 +85,8 @@ const data = {
       }
     );
   };
+
+  //TODO: Hacer que actualice la interfaz
+  //TODO: Obtner datos (codigo ciudad y aldeas) desde solicitudes o interfaz
+  //TODO: poner limite para las noches
 })();
